dd37c47e8ed119927850a4722402d29be2f80454
B19DCCN533 <quantrantc2@gmail.com>
2023-06-22 11:04:55 +0700

    .

---
+++ duoihinhbatchu/layCauhoi.php
@@ -0,0 +1,3 @@
+[{"ten":"","dapan":"nguao","anh":"https://lazi.vn/uploads/dhbc/1466667821_ngua-o.jpg"},{"ten":"","dapan":"langbam","anh":"https://cdn.lazi.vn/storage/uploads/dhbc/1469120867_lang-bam.jpg"},{"ten":"","dapan":"hoicung","anh":"https://cdn.lazi.vn/storage/uploads/dhbc/1466661239_hoi-cung.jpg"},{"ten":"","dapan":"hungthu","anh":"https://cdn.lazi.vn/storage/uploads/dhbc/1466612687_hung-thu.jpg"},{"ten":"","dapan":"baoham","anh":"https://cdn.lazi.vn/storage/uploads/dhbc/1520744162_xyl-ryfn.jpg"},{"ten":"","dapan":"xichlo","anh":"https://cdn.lazi.vn/storage/uploads/dhbc/1466667962_xich-lo.jpg"},{"ten":"","dapan":"cangua","anh":"https://cdn.lazi.vn/storage/uploads/dhbc/1466612649_ca-ngua.jpg"}]
+
+
